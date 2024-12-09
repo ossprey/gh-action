e95ffb210d072a2cafbfbd3c553dba0b7c58547d
David <31293924+dreadn0ught@users.noreply.github.com>
2024-12-09 17:05:29 +0000

    fix unit test

--- scan/test_scan.py
+++ scan/test_scan.py
@@ -3,6 +3,8 @@ from scan.scan import main
 
 
 def test_main_function():
-    # Add INPUT_FOLDER to environment variables
     os.environ["INPUT_FOLDER"] = "test/simple_math"
-    assert main() == "Success"
+    ret = main()
+    del os.environ["INPUT_FOLDER"]
+
+    assert ret == "Success"
