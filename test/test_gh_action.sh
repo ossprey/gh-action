0a717037280288e2d1dfd31024715ec5753e5ae9
dreadn0ught <31293924+dreadn0ught@users.noreply.github.com>
2025-11-06 10:27:04 +0000

    add dry run safe and malicious

--- test/test_gh_action.sh
+++ test/test_gh_action.sh
@@ -7,11 +7,12 @@ docker build -t ossprey-scan .
 export INPUT_PACKAGE="test/simple_math"
 export INPUT_MODE="python-requirements"
 export INPUT_VERBOSE=true
-export INPUT_DRY_RUN=false
+export INPUT_DRY_RUN_SAFE=false
+export INPUT_DRY_RUN_MALICIOUS=false
 
 /usr/bin/docker run --label 2a0600 --workdir /github/workspace --rm -e "pythonLocation" -e "LD_LIBRARY_PATH"  \
          -e "API_KEY=$OSSPREY_API_KEY" -e "INPUT_PACKAGE" -e "INPUT_MODE" -e "INPUT_GITHUB_COMMENTS" -e "INPUT_VERBOSE" \
-         -e "INPUT_URL" -e "INPUT_DRY_RUN" -e "HOME" \
+         -e "INPUT_URL" -e "INPUT_DRY_RUN_SAFE" -e "INPUT_DRY_RUN_MALICIOUS" -e "HOME" \
          -v "/var/run/docker.sock":"/var/run/docker.sock" -v "/workspaces/gh-action/":"/github/workspace" \
          ossprey-scan
 cd -
